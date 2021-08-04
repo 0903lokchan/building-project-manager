24579bf5d25775e4b9e614630881adf64c6fd6be
0903lokchan <0903lokchan@gmail.com>
2021-08-04 15:39:34 +1200

    updated data models to comply with requirements

--- src/app/mock_data/mock_project.ts
+++ src/app/mock_data/mock_project.ts
@@ -4,7 +4,7 @@ import { Comment } from "../data_model/comment";
 
 export const PROJECTS: Project[] = [
     {
-        id: 1,
+        id: 0,
         name: "project 1",
         status: ProjectStatus.Closed,
         startDate: "30Jul2021",
@@ -13,31 +13,14 @@ export const PROJECTS: Project[] = [
         projectManager: "Tim",
         contractor: "Gill",
         workList: [
-            {
-                status: WorkStatus.Ongoing,
-                content: "Work 1"
-            },
-            {
-                status: WorkStatus.Cancelled,
-                content: "Work 2"
-            },
-            {
-                status: WorkStatus.Scheduled,
-                content: "Work 3"
-            }
+            {status: WorkStatus.Ongoing, content: "Work 1"},
+            {status: WorkStatus.Cancelled, content: "Work 2"},
+            {status: WorkStatus.Scheduled, content: "Work 3"}
         ],
         commentList: [
-            {
-                author: "Manager",
-                content: "You may comment in here."
-            },
-            {
-                author: "Owner",
-                content: "Cool."
-            },{
-                author: "Contractor",
-                content: "Will do!"
-            }
+            {author: "Manager", content: "You may comment in here."},
+            { author: "Owner", content: "Cool." },
+            {author: "Contractor", content: "Will do!"}
         ]
     },
     {
@@ -50,35 +33,18 @@ export const PROJECTS: Project[] = [
         projectManager: "Tom",
         contractor: "George",
         workList: [
-            {
-                status: WorkStatus.Cancelled,
-                content: "Work 1"
-            },
-            {
-                status: WorkStatus.Ongoing,
-                content: "Work 2"
-            },
-            {
-                status: WorkStatus.Postponed,
-                content: "Work 3"
-            }
+            {status: WorkStatus.Cancelled, content: "Work 1"},
+            {status: WorkStatus.Ongoing, content: "Work 2"},
+            {status: WorkStatus.Postponed, content: "Work 3"}
         ],
         commentList: [
-            {
-                author: "Manager2",
-                content: "You may comment in here."
-            },
-            {
-                author: "Owner2",
-                content: "Cool."
-            },{
-                author: "Contractor2",
-                content: "Will do!"
-            }
+            {author: "Manager2", content: "You may comment in here."},
+            {author: "Owner2", content: "Cool."},
+            {author: "Contractor2", content: "Will do!"}
         ]
     },
     {
-        id: 1,
+        id: 2,
         name: "project 3",
         status: ProjectStatus.Scheduled,
         startDate: "28Jul2021",
@@ -87,31 +53,14 @@ export const PROJECTS: Project[] = [
         projectManager: "Tiff",
         contractor: "Gio",
         workList: [
-            {
-                status: WorkStatus.Scheduled,
-                content: "Work 1"
-            },
-            {
-                status: WorkStatus.Cancelled,
-                content: "Work 2"
-            },
-            {
-                status: WorkStatus.Scheduled,
-                content: "Work 3"
-            }
+            {status: WorkStatus.Scheduled, content: "Work 1"},
+            {status: WorkStatus.Cancelled, content: "Work 2"},
+            {status: WorkStatus.Scheduled, content: "Work 3"}
         ],
         commentList: [
-            {
-                author: "Manager3",
-                content: "You may comment in here."
-            },
-            {
-                author: "Owner3",
-                content: "Cool."
-            },{
-                author: "Contractor3",
-                content: "Will do!"
-            }
+            {author: "Manager3", content: "You may comment in here."},
+            {author: "Owner3", content: "Cool."},
+            {author: "Contractor3", content: "Will do!"}
         ]
     },
     
