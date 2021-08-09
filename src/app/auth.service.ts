778d08dcd56bd3f00a27c701cc324c213ed8a2a1
0903lokchan <0903lokchan@gmail.com>
2021-08-09 22:12:41 +1200

    added auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { LoginRequest } from "./data_model/loginRequest";
+import { User } from './data_model/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly USER_API = ""
+  private currentUser: User|undefined;
+
+  constructor() { }
+
+  login(loginRequest: LoginRequest): Observable<User|undefined> {
+    const users = this.getUsers()
+    const user = users.filter(user => user.username === loginRequest.username && user.password === loginRequest.password)
+    if (user.length == 1) {
+      this.currentUser = user[0];
+    }
+    return of(this.currentUser)
+  }
+
+  logout() {
+    this.currentUser = undefined;
+  }
+
+  isLoggedIn$(): Observable<boolean> {
+    return of(this.currentUser == undefined)
+  }
+
+  getCurrentUser$(): Observable<User|undefined> {
+    return of(this.currentUser)
+  }
+
+  getUsers(): User[] {
+    var users = localStorage.getItem("users");
+    if (!users) {
+      // TODO http get users
+    }
+    return users? JSON.parse(users) : []
+  }
+}
