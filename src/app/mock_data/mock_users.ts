9979d06513a9750186e93716315f290108e65b09
0903lokchan <0903lokchan@gmail.com>
2021-08-09 22:21:15 +1200

    added mock users

---
+++ src/app/mock_data/mock_users.ts
@@ -0,0 +1,24 @@
+import { User } from '../data_model/user';
+
+export const USERS: User[] = [
+	{
+		username: 'testUser',
+		password: 'test',
+		type: 'test'
+	},
+	{
+		username: 'manager',
+		password: 'password',
+		type: 'manager'
+	},
+	{
+		username: 'owner',
+		password: 'password',
+		type: 'owner'
+	},
+	{
+		username: 'contractor',
+		password: 'guesswhat',
+		type: 'contractor'
+	}
+];
