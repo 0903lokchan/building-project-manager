10a6227229e1cb754a5df95004872a3e7a892671
0903lokchan <0903lokchan@gmail.com>
2021-08-10 17:16:43 +1200

    *ngFor in buildingView

--- src/app/building/building.component.ts
+++ src/app/building/building.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BUILDINGS } from "../mock_data/mock-buildings"
+import { PROJECTS } from "../mock_data/mock_project";
 
 @Component({
   selector: 'app-building',
@@ -9,10 +10,12 @@ import { BUILDINGS } from "../mock_data/mock-buildings"
 
 export class BuildingComponent implements OnInit {
   build = BUILDINGS[0];
+  projects = PROJECTS;
   constructor() { 
 
   }
   ngOnInit(): void {
   }
+
 
 }
