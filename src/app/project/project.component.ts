dbb168518a17a8b7d86b0997ee249b706a415d07
0903lokchan <0903lokchan@gmail.com>
2021-08-12 19:32:30 +1200

    added workList buttons funtionalities

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProjectService } from '../project.service';
 import { Project, ProjectStatus } from '../data_model/project';
 import { AuthService } from '../auth.service';
 import { User } from '../data_model/user';
-import { WorkStatus } from "../data_model/work";
+import { Work, WorkStatus } from '../data_model/work';
 import { ActivatedRoute } from '@angular/router';
+import { MatTable } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project',
@@ -15,7 +18,7 @@ export class ProjectComponent implements OnInit {
   constructor(
     private projectService: ProjectService,
     private authService: AuthService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
   ) {}
   projectID!: string;
   workColumns: string[] = ['content', 'status'];
@@ -24,8 +27,15 @@ export class ProjectComponent implements OnInit {
   editMode: boolean = false;
   editProject?: Project;
   currentUser!: User;
+  @ViewChild(MatTable) workList!: MatTable<Work>;
   // I know it's ugly but it's the only way I could make select in mat-table work
-  workStatus = [WorkStatus.Ongoing, WorkStatus.Cancelled, WorkStatus.Done, WorkStatus.Postponed, WorkStatus.Scheduled];
+  workStatus = [
+    WorkStatus.Ongoing,
+    WorkStatus.Cancelled,
+    WorkStatus.Done,
+    WorkStatus.Postponed,
+    WorkStatus.Scheduled,
+  ];
 
   getProject(): void {
     this.projectService.getProject(this.projectID).subscribe((project) => {
@@ -33,16 +43,18 @@ export class ProjectComponent implements OnInit {
     });
   }
 
-  sendProject(project: Project): void {
-    this.projectService.updateProject(this.project).subscribe((success) => {
-      if (success) {
-        // TODO create and use message service
-        alert('Successfully updated project.');
-        this.getProject();
-      } else {
-        alert('Update failed.');
-      }
-    });
+  sendProject(project: Project): Observable<boolean> {
+    return this.projectService.updateProject(project).pipe(
+      tap(success => {
+        if (success) {
+          // TODO create and use message service
+          alert('Successfully updated project.');
+          this.getProject();
+        } else {
+          alert('Update failed.');
+        }
+      })
+    )
   }
 
   enterEdit(): void {
@@ -56,7 +68,7 @@ export class ProjectComponent implements OnInit {
 
   exitEdit(): void {
     if (confirm('Save changes?')) {
-      this.sendProject(this.editProject!);
+      this.sendProject(this.editProject!).subscribe();
     }
     this.editMode = false;
     delete this.editProject;
@@ -66,30 +78,72 @@ export class ProjectComponent implements OnInit {
     if (!confirm('Confirm mark project as archived?')) {
       return;
     }
-    this.project.status = ProjectStatus.Closed;
-    this.sendProject(this.project);
+    const project = { ...this.project }
+    project.status = ProjectStatus.Closed
+    this.sendProject(project).subscribe();
   }
 
   open(): void {
     if (!confirm('Confirm mark project as current?')) {
       return;
     }
-    this.project.status = ProjectStatus.Current;
-    this.sendProject(this.project);
+    const project = { ...this.project }
+    project.status = ProjectStatus.Current
+    this.sendProject(project).subscribe();
   }
 
   close(): void {
     if (!confirm('Confirm mark project as closed?')) {
       return;
     }
-    this.project.status = ProjectStatus.Closed;
-    this.sendProject(this.project);
+    const project = { ...this.project }
+    project.status = ProjectStatus.Closed
+    this.sendProject(project).subscribe();
+  }
+
+  createWork(): void {
+    const newWork = prompt('Please enter name for the new work') || '';
+    const project = {...this.project}
+    project.workList.push({
+      content: newWork,
+      status: WorkStatus.Scheduled,
+    });
+    this.sendProject(project).subscribe(success => {
+      if (success) {
+        this.workList.renderRows();
+      }
+    })
+  }
+
+  deleteWork(id: number): void {
+    const project = {...this.project}
+    project.workList.splice(id, 1);
+    this.sendProject(project).subscribe(success => {
+      if (success) {
+        this.workList.renderRows();
+      }
+    })
+  }
+
+  changeWorkStatus(): void {
+    this.sendProject(this.project).subscribe(success => {
+      if (success) {
+        this.workList.renderRows();
+      }
+    })
   }
 
   ngOnInit(): void {
     this.authService.getCurrentUser$().subscribe((user) => {
       if (user) {
         this.currentUser = user;
+      } else {
+        //test user
+        this.currentUser = {
+          username: 'manager',
+          password: 'password',
+          type: 'manager',
+        };
       }
     });
     this.projectID = this.route.snapshot.paramMap.get('id') || '0';
