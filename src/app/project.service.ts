9112fb4ff28e4b3cca579e72a88938415ab286ab
0903lokchan <0903lokchan@gmail.com>
2021-08-12 19:19:41 +1200

    updated caching

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Project } from "./data_model/project";
 import { PROJECTS } from "./mock_data/mock_project";
 import { Observable, of } from "rxjs";
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,30 @@ export class ProjectService {
 
   constructor() { }
 
+  getProjects(): Project[] {
+    const storedProjects = localStorage.getItem('projects')
+    const projects: Project[] = storedProjects ? JSON.parse(storedProjects) : PROJECTS
+    return projects;
+  }
+
   getProject(id: string): Observable<Project> {
-    const storedProject = localStorage.getItem('project')
-    const project: Project[] = storedProject? JSON.parse(storedProject) : PROJECTS
-    return of(project!.filter(project => project.id == id)[0]);
+    return of(this.getProjects().filter(project => project.id == id)[0])
   }
 
   updateProject(project: Project): Observable<boolean> {
-    // TODO make HTTP request
-    localStorage.setItem('project', JSON.stringify(project))
-    return of(true);
+    try {
+      // TODO make HTTP request
+      const projects = this.getProjects()
+      const id = projects.findIndex(proj => proj.id == project.id)
+      if (id == -1) {
+        projects.push(project);
+      } else {
+        projects[id] = project;
+      }
+      localStorage.setItem('projects', JSON.stringify(projects))
+      return of(true);
+    } catch (e) {
+      return of(false)
+    }
   }
 }
