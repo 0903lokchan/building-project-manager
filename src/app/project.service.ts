181e81fba9ed78ed52229190255fa90293b6c365
Lok Chan <0903lokchan@gmail.com>
2021-08-17 21:46:46 +1200

    update project service structure

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,40 +1,83 @@
 import { Injectable } from '@angular/core';
-import { Project } from "./data_model/project";
-import { PROJECTS } from "./mock_data/mock_project";
-import { Observable, of } from "rxjs";
+import { Project } from './data_model/project';
+import { PROJECTS } from './mock_data/mock_project';
+import { Observable, of } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+const USE_MOCK: boolean = true;
+const STORAGE_KEY: string = "projects";
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
-
-  constructor() { }
+  constructor() {}
 
   getProjects(): Project[] {
-    const storedProjects = localStorage.getItem('projects')
-    const projects: Project[] = storedProjects ? JSON.parse(storedProjects) : PROJECTS
+    // retrieve session storage
+    const storedProjects = sessionStorage.getItem('projects');
+    if (storedProjects) {
+      return JSON.parse(storedProjects);
+    }
+    // use mock data
+    if (USE_MOCK) {
+      sessionStorage.setItem(STORAGE_KEY, JSON.stringify(PROJECTS))
+      return PROJECTS;
+    }
+    // retrieve from API
+    const projects = this.httpGetProjects()
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(projects))
     return projects;
   }
 
-  getProject(id: string): Observable<Project> {
-    return of(this.getProjects().filter(project => project.id == id)[0])
+  setProjects(projects: Project[]): void {
+    if (USE_MOCK) {
+      sessionStorage.setItem(STORAGE_KEY, JSON.stringify(projects));
+    } else {
+      // update session storage when request was successful
+      if (this.httpSetProjects(projects)) {
+        sessionStorage.setItem(STORAGE_KEY, JSON.stringify(projects));
+      }
+    }
+  }
+
+  getProject(id: string): Observable<Project | null> {
+    const project = this.getProjects().filter((project) => project.id == id);
+    if (project.length == 1) {
+      return of(project[0]);
+    }
+    return of(null);
   }
 
   updateProject(project: Project): Observable<boolean> {
     try {
-      // TODO make HTTP request
-      const projects = this.getProjects()
-      const id = projects.findIndex(proj => proj.id == project.id)
+      const projects = this.getProjects();
+      const id = projects.findIndex((proj) => proj.id == project.id);
       if (id == -1) {
+        // create project
         projects.push(project);
       } else {
+        // update project
         projects[id] = project;
       }
-      localStorage.setItem('projects', JSON.stringify(projects))
+      this.setProjects(projects)
       return of(true);
     } catch (e) {
-      return of(false)
+      return of(false);
+    }
+  }
+
+  httpGetProjects(): Project[] {
+    // TODO make HTTP request
+    return [];
+  }
+
+  httpSetProjects(projects: Project[]): boolean {
+    // TODO make HTTP request
+    try {
+      return true;
+    } catch (error) {
+      return false;
     }
   }
 }
