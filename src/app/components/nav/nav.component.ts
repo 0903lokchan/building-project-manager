2b46fac31c5559b58b64161bf7aae248814a980e
Lok Chan <0903lokchan@gmail.com>
2021-10-03 09:15:40 +1300

    back button; workList display fix

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -5,29 +5,40 @@ import { map, shareReplay } from 'rxjs/operators';
 import { User } from '../../data_model/user';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  user: User|undefined;
+  user: User | undefined;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService,
+    private router: Router,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
-    this.authService.getCurrentUser$().subscribe(user => this.user = user)
+    this.authService.getCurrentUser$().subscribe((user) => (this.user = user));
   }
 
   logout(): void {
-    this.authService.logout()
-    this.router.navigate(["../login"])
+    this.authService.logout();
+    this.router.navigate(['../login']);
+  }
+
+  back(): void {
+    this.location.back();
   }
 }
